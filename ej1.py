05e6e66d71773406e0118dfe3e310ddbea0de999
juditgarciap <judit.garciapadronleon@usp.ceu.es>
2022-10-25 15:09:56 +0200

    ñ

--- ej1.py
+++ ej1.py
@@ -1,4 +1,11 @@
 class vehiculo () :
-    def _innit__ (self, color,rueda):
+    def __innit__ (self, color,rueda):
         self.color = color
         self.rueda = rueda
+    
+class Coche (vehiculo):
+    def _init_ (self.color,rueda,velocidad,cilindrada):  
+    vehiculo(). __init__(self,color,rueda)
+    self.
+
+
