7e63572bb438ac44b35e6cb7dc5eadbe301cf217
juditgarciap <judit.garciapadronleon@usp.ceu.es>
2022-10-25 15:12:45 +0200

    CLASES

--- ej1.py
+++ ej1.py
@@ -1,11 +1,13 @@
-class vehiculo () :
+class vehiculo():
     def __innit__ (self, color,rueda):
         self.color = color
         self.rueda = rueda
     
 class Coche (vehiculo):
-    def _init_ (self.color,rueda,velocidad,cilindrada):  
-    vehiculo(). __init__(self,color,rueda)
-    self.
+    def __init__(self, color,rueda,velocidad,cilindrada):  
+        vehiculo(). __init__(self,color,rueda)
+        self.velocidad= velocidad  
+        self.cilindrada= cilindrada
+     
 
 
